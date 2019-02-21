f1823cf946f7e921464e351365ea53cb64029957
Adam Berlin <aberlin@pivotal.io>
2019-02-21 00:51:10 +0000

    Try to clean up storage for concourse search.

--- concourse_search/concourse.py
+++ concourse_search/concourse.py
@@ -31,7 +31,6 @@ class FlyWatchResponse():
     def __init__(self, raw_lines, was_success):
         self._raw_lines = raw_lines
         self._was_success = was_success
-        self._logfile = logfile
 
     def raw_lines(self):
         return self._raw_lines
@@ -106,7 +105,6 @@ class ConcourseBaseUrlFinder():
 
     def _fetch(self, target):
         fly_target = self._fly.target_matching(target)
-        
         self._cache[target] = fly_target.url()
         return fly_target.url()
 
@@ -158,12 +156,65 @@ class ConcourseBuild():
             build_number=self.build_number()-1,
             base_url=self.base_url(),
         )
+
+    
+class ConcourseSearchStorage():
+    def ensure_storage_exists(self):
+        if not os.path.exists("/tmp/.concourse-search"):
+            os.makedirs("/tmp/.concourse-search")
+
+    def logfile_path(self, concourse_build):
+        return "/tmp/.concourse-search/{target}-{pipeline}-{job}-{build}.log".format(
+            target=concourse_build.target(),
+            pipeline=concourse_build.pipeline().replace("_", "-"),
+            job=concourse_build.job().replace("_", "-").replace("/", "-"),
+            build=concourse_build.build_number(),
+        )
+    def success_file_path(self, concourse_build):
+        return "/tmp/.concourse-search/{target}-{pipeline}-{job}-{build}-was-success.log".format(
+            target=concourse_build.target(),
+            pipeline=concourse_build.pipeline().replace("_", "-"),
+            job=concourse_build.job().replace("_", "-").replace("/", "-"),
+            build=concourse_build.build_number(),
+        )
+
+    def contains(self, concourse_build):
+        return os.path.exists(self.logfile_path(concourse_build))
+
+    def store(self, concourse_build, response):
+        logfile_path = self.logfile_path(concourse_build)
+        success_file_path = self.success_file_path(concourse_build)
+        
+        with open(logfile_path, "wb") as logfile:
+            logfile.write(response.raw_lines())
+
+        if response.was_success():
+            with open(success_file_path, "w") as logfile:
+                logfile.write(u"true")
+
+    def retrieve(self, concourse_build):
+        logfile_path = self.logfile_path(concourse_build)
+        success_file_path = self.success_file_path(concourse_build)
+        
+        with open(logfile_path, "rb") as file:
+            raw_lines = file.read()
+            
+        was_success = os.path.exists(success_file_path)
+
+        return BuildResponse(
+            raw_lines=raw_lines,
+            was_success=was_success,
+            logfile_path=logfile_path,
+        )
+
     
 class ConcourseSearch():
 
     def __init__(self, fly, logger=default_logger):
         self.logger = logger
         self.concourse_base_url_finder = ConcourseBaseUrlFinder(fly=fly)
+        self._storage = ConcourseSearchStorage()
+        self._fly = fly
 
     def find_builds(self, target, pipeline, job, starting_build_number, limit=100):
         base_url = self._get_base_url(target)
@@ -200,8 +251,14 @@ class ConcourseSearch():
     
     def find(self, target, pipeline, job, build):
         self.logger("Searching for build number: {build}".format(build=build))
-        base_url = self._get_base_url(target)        
-        concourse_build = ConcourseBuild(target, pipeline, job, build, base_url)
+        base_url = self._get_base_url(target)
+        concourse_build = ConcourseBuild(
+            target=target,
+            pipeline=pipeline,
+            job=job,
+            build_number=build,
+            base_url=base_url
+        )
         raw_lines = self._fetch(concourse_build).raw_lines()
 
         return transform_lines(
@@ -214,49 +271,20 @@ class ConcourseSearch():
         return self.concourse_base_url_finder.find(target)
     
     def _fetch(self, concourse_build):
-        if not os.path.exists("/tmp/.concourse-search"):
-            os.makedirs("/tmp/.concourse-search")
-
-        logfile_path = "/tmp/.concourse-search/{target}-{pipeline}-{job}-{build}.log".format(
-            target=concourse_build.target(),
-            pipeline=concourse_build.pipeline().replace("_", "-"),
-            job=concourse_build.job().replace("_", "-").replace("/", "-"),
-            build=concourse_build.build_number(),
-        )
+        self._storage.ensure_storage_exists()
         
-        success_file_path = "/tmp/.concourse-search/{target}-{pipeline}-{job}-{build}-was-success.log".format(
-            target=concourse_build.target(),
-            pipeline=concourse_build.pipeline().replace("_", "-"),
-            job=concourse_build.job().replace("_", "-").replace("/", "-"),
-            build=concourse_build.build_number(),
-        )
+        logfile_path = self._storage.logfile_path(concourse_build)
+        success_file_path = self._storage.success_file_path(concourse_build)
 
-        if not os.path.exists(logfile_path):
+        if not self._storage.contains(concourse_build):
             response = self._do_fetch(concourse_build)
-            
-            with open(logfile_path, "wb") as logfile:
-                logfile.write(response.raw_lines())
+            self._storage.store(concourse_build, response)
 
-            if response.was_success():
-                with open(success_file_path, "w") as logfile:
-                    logfile.write(u"true")
-
-            return response
-        else:
-            with open(logfile_path, "rb") as file:
-                raw_lines = file.read()
-
-            was_success =  os.path.exists(success_file_path)
-
-            return BuildResponse(
-                raw_lines=raw_lines,
-                was_success=was_success,
-                logfile_path=logfile_path,
-            )
+        return self._storage.retrieve(concourse_build)
 
     def _do_fetch(self, concourse_build):
         self.logger("Searching concourse for build number: {build}".format(
-            build=concourse_build.build()
+            build=concourse_build.build_number()
         ))
 
         return self._fly.watch(
